47d1461295af9ac3768a32aa524536f68ecb2f23
R Q <chatterjeebiswarup61@gmail.com>
2025-11-06 23:49:22 +0530

    removed

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
     try {
-        const uri = "mongodb+srv://chatterjeebiswarup61_db_user:WFC6f40eoXprNTqP@cluster0.j6mbvym.mongodb.net/?appName=Cluster0";
+        const uri = process.env.MONGO_URI;
         await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
